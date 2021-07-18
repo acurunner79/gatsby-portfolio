08c454ecaa5c61717cd8ca273f30dd851ad1ee1e
Jorge <djsmoothzouk@gmail.com>
2021-07-17 20:59:18 -0500

    ready for deployment

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
-// import 'popper.js/dist/popper.min'
-import 'bootstrap/dist/js/bootstrap.min.js'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
