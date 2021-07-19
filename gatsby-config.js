bfb0c6a04a380f3e561a6b1eebd19cc9d0f4e305
Jorge <djsmoothzouk@gmail.com>
2021-07-18 20:17:43 -0500

    attempt to fix missing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `I like Google fonts`
+    title: `Jorge Soto: Fullstack Web Developer`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     {
@@ -17,17 +20,6 @@ module.exports = {
         display: 'swap'
       },
     },
-  ]
-}
-
-module.exports = {
-  siteMetadata: {
-    title: `Jorge Soto: Fullstack Web Developer`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
