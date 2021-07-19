5e5c1f896c565aa90d37408771560716487f77f3
Jorge <djsmoothzouk@gmail.com>
2021-07-18 20:42:24 -0500

    attempt to fix missing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
+// require("dotenv").config({
+//   path: `.env`,
+// });
 require("dotenv").config({
-  path: `.env`,
-});
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
