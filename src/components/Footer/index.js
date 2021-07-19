5455bb193c3f69c70053195eacb303c102b782e9
Jorge <djsmoothzouk@gmail.com>
2021-07-18 21:29:40 -0500

    completed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,9 +6,9 @@ import '../../styles/footer.css'
 const Footer = () => {
     return (
         <div className="footer-main">
-            <img id="footer-icon"src={logo} alt="footer-logo"/>
+            <img id="footer-icon" src={logo} alt="footer-logo"/>
             <div>
-                <label>Copyright</label>
+                <label id="footer-text">&copy;Copyright 2021 This site was created by Jorge Soto</label>
             </div>
                 
         </div>
