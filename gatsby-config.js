3f86ca6747a353af04c5a12db701855c5b02a6c0
Jorge <djsmoothzouk@gmail.com>
2021-07-18 20:31:17 -0500

    attempt to fix missing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,14 @@ module.exports = {
         display: 'swap'
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+          host: process.env.CONTENTFUL_HOST,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
@@ -31,14 +39,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-          spaceId: process.env.CONTENTFUL_SPACE_ID,
-          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-          host: process.env.CONTENTFUL_HOST,
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
