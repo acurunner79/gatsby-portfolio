b1b7130ae50c477041791c4975e71dccf4ed7db6
Jorge <djsmoothzouk@gmail.com>
2021-07-20 15:40:50 -0500

    adding title to blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
       options: {
           spaceId: process.env.CONTENTFUL_SPACE_ID,
           accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-          host: process.env.CONTENTFUL_HOST,
+          host: 'preview.contentful.com',
       },
     },
     `gatsby-plugin-react-helmet`,
