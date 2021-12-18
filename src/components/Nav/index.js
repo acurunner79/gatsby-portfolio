079c42e58ee0126ce993e2afb859e0469c0a36a0
acurunner79 <djsmoothzouk@gmail.com>
2021-12-18 15:57:00 -0600

    updated link from logo to home page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,7 +8,7 @@ import '../../styles/Nav.css'
 const Nav = () => {
     return ( 
         <div className="_nav">
-           <Link to="/main">
+           <Link to="/">
               <img className="navlogo" src="https://res.cloudinary.com/acurunner79/image/upload/c_scale,w_250/v1616622334/website_logo_transparent_background_lzgkuz.png" alt="jorge soto.coder"></img>
            </Link>
            <div className="menu-bar">
